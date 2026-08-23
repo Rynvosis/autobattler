96ae6c8481067137fd4f945c5b6a017d0dbe4f7d
Ryn Easterbrook <rynsterbrook@gmail.com>
2026-08-23 23:54:23 +0100

    Split unit triggers by the participant they test

--- api/Api.Combat.Tests/TriggerTests.cs
+++ api/Api.Combat.Tests/TriggerTests.cs
@@ -1,40 +1,47 @@
 using Api.Combat.Abilities;
 using Api.Combat.Abilities.Scopes;
-using Api.Combat.Events;
 
 namespace Api.Combat.Tests;
 
 public class TriggerTests
 {
-    private static BattleEvent Hurt(Board board, int targetId) =>
-        new UnitHurtEvent { Source = Boards.Find(board, 1), Target = Boards.Find(board, targetId), Value = 1 };
-
     public static TheoryData<Trigger, int, Func<Board, BattleEvent>, bool> Cases => new()
     {
         { new RoundTrigger<StartEvent>(), 0, _ => new StartEvent(), true },
         { new RoundTrigger<StartEvent>(), 0, board => Hurt(board, 0), false },
         {
-            new UnitTrigger<UnitHurtEvent> { Scopes = [new SelfScope()] },
+            new TargetTrigger<UnitHurtEvent> { Scopes = [new SelfScope()] },
             0, board => Hurt(board, 0), true
         },
         {
-            new UnitTrigger<UnitHurtEvent> { Scopes = [new SelfScope()] },
+            new TargetTrigger<UnitHurtEvent> { Scopes = [new SelfScope()] },
             0, board => Hurt(board, 2), false
         },
         {
-            new UnitTrigger<UnitHurtEvent>
+            new TargetTrigger<UnitHurtEvent>
             {
                 Scopes = [new HeadScope { Side = ScopeSide.Ally, Range = ScopeRange.From(0) }]
             },
             0, board => Hurt(board, 2), true
         },
         {
-            new UnitTrigger<UnitHurtEvent> { Scopes = [new SelfScope()] },
+            new TargetTrigger<UnitHurtEvent> { Scopes = [new SelfScope()] },
             0,
             board => new UnitAttackEvent { Source = Boards.Find(board, 1), Target = Boards.Find(board, 0), Value = 1 },
             false
         },
+        {
+            new SourceTrigger<UnitAttackEvent> { Scopes = [new SelfScope()] },
+            0,
+            board => new UnitAttackEvent { Source = Boards.Find(board, 0), Target = Boards.Find(board, 1), Value = 1 },
+            true
+        }
     };
+
+    private static BattleEvent Hurt(Board board, int targetId)
+    {
+        return new UnitHurtEvent { Source = Boards.Find(board, 1), Target = Boards.Find(board, targetId), Value = 1 };
+    }
 
     [Theory]
     [MemberData(nameof(Cases))]
