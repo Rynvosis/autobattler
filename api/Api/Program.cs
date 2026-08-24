9a6a0a9c828543866a46787a34adb35d4aac97e6
Ryn Easterbrook <rynsterbrook@gmail.com>
2026-08-24 18:38:20 +0100

    Connect the API to DynamoDB and create the runs table

--- api/Api/Program.cs
+++ api/Api/Program.cs
@@ -1,11 +1,62 @@
-var builder = WebApplication.CreateBuilder(args);
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Extensions.NETCore.Setup;
+using Amazon.Runtime;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+AWSOptions options = builder.Configuration.GetAWSOptions();
+string? serviceUrl = builder.Configuration["DynamoDB:ServiceUrl"];
+
+if (!string.IsNullOrEmpty(serviceUrl))
+{
+    options.DefaultClientConfig.ServiceURL = serviceUrl;
+    options.Credentials = new BasicAWSCredentials("accessKey", "secretKey");
+}
+
+builder.Services.AddDefaultAWSOptions(options);
+builder.Services.AddAWSService<IAmazonDynamoDB>();
+
+WebApplication app = builder.Build();
+
+const string tableName = "runs";
+
+// Table creation at startup
+IAmazonDynamoDB dynamoDB = app.Services.GetRequiredService<IAmazonDynamoDB>();
+
+try
+{
+    await dynamoDB.DescribeTableAsync(tableName);
+}
+catch (ResourceNotFoundException)
+{
+    await dynamoDB.CreateTableAsync(new CreateTableRequest
+    {
+        TableName = tableName,
+        AttributeDefinitions =
+        [
+            new AttributeDefinition
+            {
+                AttributeName = "runId",
+                AttributeType = ScalarAttributeType.S
+            }
+        ],
+        KeySchema =
+        [
+            new KeySchemaElement
+            {
+                AttributeName = "runId",
+                KeyType = KeyType.HASH
+            }
+        ],
+        BillingMode = BillingMode.PAY_PER_REQUEST
+    });
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -16,29 +67,4 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-    {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
